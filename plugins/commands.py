0766c9a0be13a33b736ea556b5a7ea25880b4e8a
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-08-12 22:29:13 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -59,7 +59,7 @@ async def start(bot, cmd):
                 f_caption=files.caption
                 if CUSTOM_FILE_CAPTION:
                     try:
-                        f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+                        f_caption="@on air movies",
                     except Exception as e:
                         print(e)
                         f_caption=f_caption
